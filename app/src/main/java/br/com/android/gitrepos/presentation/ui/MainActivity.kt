d3e7eaad86ab3e1c8aea58504334a16b08738f06
kadusouza <kdusouza@gmail.com>
2022-07-15 15:53:59 -0300

    Inclusão do fragment e navigation

--- app/src/main/java/br/com/android/gitrepos/presentation/ui/MainActivity.kt
+++ app/src/main/java/br/com/android/gitrepos/presentation/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import br.com.android.gitrepos.R
 import br.com.android.gitrepos.data.ResponseStatus
 import br.com.android.gitrepos.data.model.Item
 import br.com.android.gitrepos.databinding.ActivityMainBinding
@@ -20,148 +21,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: GitViewModel by viewModel()
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var reposAdapter: GitRepoAdapter
-
-    private var permissionDenied = false
-    private val listRepos: MutableList<Item> = mutableListOf()
-    private var loading = false
-    private var initialSize = 0
-    private var page = 1
-
-    private val RECYCLER_STATE = "recycler_state"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        if (savedInstanceState != null) {
-            binding.rvRepoInfo.layoutManager?.onRestoreInstanceState(savedInstanceState.getParcelable(RECYCLER_STATE))
-        }
-        setObservable()
-        getData()
-    }
-
-    private fun getData() {
-        val gson = Gson()
-        val cachedRepos = RepoCache.read(applicationContext)
-        if (cachedRepos != "") {
-            val cd = gson.fromJson(cachedRepos, CacheData::class.java)
-            page = cd.page
-            listRepos.addAll(cd.items)
-            setReposList(listRepos)
-        } else {
-            viewModel.getRepos(page)
-        }
-    }
-
-    private fun setObservable() {
-        viewModel.state.observe(this) {
-            when (it.status) {
-                ResponseStatus.SUCCESS -> {
-                    val repos = it.data
-                    repos?.let { g ->
-                        if (listRepos.size == 0) {
-                            listRepos.addAll(g.items)
-                            setReposList(listRepos)
-                        } else {
-                            listRepos.addAll(g.items)
-                            reposAdapter.notifyItemRangeInserted(initialSize, listRepos.size)
-                        }
-
-                        val gson = Gson()
-                        val cacheData = CacheData(
-                            page = page,
-                            items = listRepos
-                        )
-                        RepoCache.save(applicationContext, gson.toJson(cacheData))
-                        binding.pbGitrepos.visibility = View.GONE
-                        loading = false
-                    }
-                }
-                ResponseStatus.LOADING -> {
-                    loading = true
-                    if (listRepos.size == 0) {
-                        binding.pbGitrepos.visibility = View.VISIBLE
-                    }
-                }
-                ResponseStatus.ERROR -> {
-                    binding.pbGitrepos.visibility = View.GONE
-                    it.message?.let { it1 -> showMessage(it1) }
-                }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        viewModel.state.removeObservers(this)
-    }
-
-    private fun setReposList(list: List<Item>) {
-        binding.apply {
-            reposAdapter = GitRepoAdapter(list)
-            rvRepoInfo.adapter = reposAdapter
-
-            val lManager = rvRepoInfo.layoutManager as LinearLayoutManager
-
-            rvRepoInfo.addOnScrollListener(object: RecyclerView.OnScrollListener(){
-                var visibleItemCount = 0
-                var totalItemCount = 0
-                var firstVisibleItem = 0
-                var lastCompletelyVisibleItem = 0
-
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
-
-                    visibleItemCount = rvRepoInfo.childCount
-                    totalItemCount = lManager.itemCount
-                    firstVisibleItem = lManager.findFirstVisibleItemPosition()
-                    lastCompletelyVisibleItem = lManager.findLastVisibleItemPosition()
-
-                    if (!loading && (firstVisibleItem + (visibleItemCount*2)) >= totalItemCount - 1) {
-                        initialSize = listRepos.size
-                        page += 1
-                        loading = true
-                        viewModel.getRepos(page)
-                    }
-
-                }
-            })
-        }
-    }
-
-    private fun showMessage(msg: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Git Repos")
-        builder.setMessage(msg)
-        builder.setNeutralButton("Ok") { dialogInterface, _ ->
-            dialogInterface.dismiss()
-        }
-        val dialog = builder.create()
-        dialog.show()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == Permissions.INTERNET_PERMISSION_CODE &&
-                grantResults[0] == PackageManager.PERMISSION_DENIED && !permissionDenied) {
-            permissionDenied = true
-            Permissions.requestInternetPermission(this)
-        } else {
-            getData()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable(RECYCLER_STATE, binding.rvRepoInfo.layoutManager?.onSaveInstanceState())
-
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/br/com/android/gitrepos/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,159 @@
+package br.com.android.gitrepos.presentation.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.android.gitrepos.R
+import br.com.android.gitrepos.data.ResponseStatus
+import br.com.android.gitrepos.data.model.Item
+import br.com.android.gitrepos.databinding.FragmentHomeBinding
+import br.com.android.gitrepos.presentation.adapter.GitRepoAdapter
+import br.com.android.gitrepos.utils.CacheData
+import br.com.android.gitrepos.utils.RepoCache
+import com.google.gson.Gson
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: GitViewModel by viewModel()
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var reposAdapter: GitRepoAdapter
+
+    private val listRepos: MutableList<Item> = mutableListOf()
+    private var loading = false
+    private var initialSize = 0
+    private var page = 1
+
+    private val RECYCLER_STATE = "recycler_state"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_home, container, false).also {
+        binding = FragmentHomeBinding.bind(it)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (savedInstanceState != null) {
+            binding.rvRepoInfo.layoutManager?.onRestoreInstanceState(savedInstanceState.getParcelable(RECYCLER_STATE))
+        }
+        setObservable()
+        getData()
+    }
+
+    private fun getData() {
+        val gson = Gson()
+        val cachedRepos = RepoCache.read(requireContext())
+        if (cachedRepos != "") {
+            val cd = gson.fromJson(cachedRepos, CacheData::class.java)
+            page = cd.page
+            listRepos.addAll(cd.items)
+            setReposList(listRepos)
+        } else {
+            viewModel.getRepos(page)
+        }
+    }
+
+    private fun setObservable() {
+        viewModel.state.observe(viewLifecycleOwner) {
+            when (it.status) {
+                ResponseStatus.SUCCESS -> {
+                    val repos = it.data
+                    repos?.let { g ->
+                        if (listRepos.size == 0) {
+                            listRepos.addAll(g.items)
+                            setReposList(listRepos)
+                        } else {
+                            listRepos.addAll(g.items)
+                            reposAdapter.notifyItemRangeInserted(initialSize, listRepos.size)
+                        }
+
+                        val gson = Gson()
+                        val cacheData = CacheData(
+                            page = page,
+                            items = listRepos
+                        )
+                        RepoCache.save(requireContext(), gson.toJson(cacheData))
+                        binding.pbGitrepos.visibility = View.GONE
+                        loading = false
+                    }
+                }
+                ResponseStatus.LOADING -> {
+                    loading = true
+                    if (listRepos.size == 0) {
+                        binding.pbGitrepos.visibility = View.VISIBLE
+                    }
+                }
+                ResponseStatus.ERROR -> {
+                    binding.pbGitrepos.visibility = View.GONE
+                    it.message?.let { it1 -> showMessage(it1) }
+                }
+            }
+        }
+    }
+
+    private fun setReposList(list: List<Item>) {
+        binding.apply {
+            reposAdapter = GitRepoAdapter(list)
+            rvRepoInfo.adapter = reposAdapter
+
+            val lManager = rvRepoInfo.layoutManager as LinearLayoutManager
+
+            rvRepoInfo.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+                var visibleItemCount = 0
+                var totalItemCount = 0
+                var firstVisibleItem = 0
+                var lastCompletelyVisibleItem = 0
+
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    visibleItemCount = rvRepoInfo.childCount
+                    totalItemCount = lManager.itemCount
+                    firstVisibleItem = lManager.findFirstVisibleItemPosition()
+                    lastCompletelyVisibleItem = lManager.findLastVisibleItemPosition()
+
+                    if (!loading && (firstVisibleItem + (visibleItemCount*2)) >= totalItemCount - 1) {
+                        initialSize = listRepos.size
+                        page += 1
+                        loading = true
+                        viewModel.getRepos(page)
+                    }
+
+                }
+            })
+        }
+    }
+
+    private fun showMessage(msg: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Git Repos")
+        builder.setMessage(msg)
+        builder.setNeutralButton("Ok") { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.state.removeObservers(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(RECYCLER_STATE, binding.rvRepoInfo.layoutManager?.onSaveInstanceState())
+    }
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+}
